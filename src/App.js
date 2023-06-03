4d7485d695faff3b423fb3b75e8145026cb2e563
TheGreaterGoo <thegreatergoo@gmail.com>
2023-06-03 14:22:12 -0700

    Express installation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import About from './components/About.js';
+import LayerMask from './components/LayerMask.js';
+import SignPage from './components/SignPage.js';
 
 function App() {
   return (
     <div className="App">
       <p>hi</p>
+      <About />
+      <LayerMask />
+      <SignPage />
     </div>
   );
 }
